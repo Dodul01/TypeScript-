11cfb140ed881eda035f263e914a5ada716c3614
Dodul01 <mhdodul299@gmail.com>
2024-11-05 14:01:20 +0600

    Class and object module added

--- moduleThree/classAndObject.ts
+++ moduleThree/classAndObject.ts
@@ -1,4 +1,41 @@
 {
     // Class and Object
+    class Animal {
+        name: string;
+        species: string;
+        sound: string;
+
+        constructor(name: string, species: string, sound: string) {
+            this.name = name;
+            this.species = species;
+            this.sound = sound;
+        }
+
+        makeSound() {
+            console.log(`The ${this.name} says ${this.sound}`);
+        }
+
+    }
+
+    const dog = new Animal('Bangla Dog', 'Dog', 'Ghaw Ghaw');
+    const cat = new Animal('Pussy Cat', 'cat', 'Meaw Meaw');
+    // console.log(dog.sound);
+    // console.log(cat.sound);
+    // dog.makeSound();
+    // cat.makeSound();
+
+    // Modefier
+    class AnimalNew {
+        constructor(public name: string, public species: string, public sound: string) { }
     
+        makeSond(){
+            console.log(`The ${this.name} says ${this.sound}.`);
+        }
+    }
+
+    const dogNew = new AnimalNew('Dog', 'dog', 'ghew')
+    const catNew = new AnimalNew('Cat', 'cat', 'meaw')
+    // console.log(dogNew.name);
+    // console.log(catNew.sound);
+    // dogNew.makeSond()
 }
